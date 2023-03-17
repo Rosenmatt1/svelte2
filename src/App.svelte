404c234730372459e1e8e788559db1975a02b57b
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-03-16 18:55:56 -0600

    Created FeedbackList and dumby data

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,27 @@
 <script>
-	export let name;
+	import FeedbackList from './components/FeedbackList.svelte'
+
+	let feedback = [
+		{
+			id: 1,
+		rating: 10,
+		text: "Lorem Ipsum was the best I have ever tasted"
+		},
+		{
+			id: 2,
+		rating: 8,
+		text: "Overall it was good.  A bit pricey"
+		},
+		{
+			id: 3,
+		rating: 3,
+		text: "The worst around"
+		}
+	]
 </script>
 
+<!-- if passing a prop that is named the same, don't need to name it -->
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<FeedbackList {feedback}/>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let feedback = []
+
+</script>
+
+{#each feedback as fb (fb.id)}
+<h3> {fb.text} </h3>
+{/each}
