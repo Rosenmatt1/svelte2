b96cd2d3d78b50fb95088d0a01741bf28a0135e4
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-03-17 22:16:57 -0600

    Created cunt and average variables in App

--- src/App.svelte
+++ src/App.svelte
@@ -18,10 +18,18 @@
 		text: "The worst around"
 		}
 	]
+
+	$: count = feedback.length
+	$: average = feedback.reduce((a, {rating}) => 0) / count 
+
+	const deleteFeedback = (e) => {
+		const itemId = e.detail
+		feedback = feedback.filter((item) =>item.id != itemId)
+	}
 </script>
 
 <!-- if passing a prop that is named the same, don't need to name it -->
 <main class="container">
-	<FeedbackList {feedback}/>
+	<FeedbackList {feedback} on:delete-feedback={deleteFeedback}/>
 </main>
 
