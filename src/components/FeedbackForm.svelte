12697035f77dc035c77336d034d1e05f425eddca
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-03-18 19:55:06 -0600

    Got the app working again

--- src/components/FeedbackForm.svelte
+++ src/components/FeedbackForm.svelte
@@ -1,37 +1,37 @@
 <script>
-  import {v4 as uuidv4} from 'uuid'
-  import {FeedbackStore} from '../stores'
+  // import {v4 as uuidv4} from 'uuid'
+  // import {FeedbackStore} from '../stores'
   import Card from './Card.svelte'
   import Button from './Button.svelte'
-  import RatingSelect from './RatingSelect.svelte'
+  // import RatingSelect from './RatingSelect.svelte'
   let text = ''
-  let rating = 10
+  // let rating = 10
   let btnDisabled = true
-  let min = 10
-  let message
-  const handleSelect = e => rating = e.detail
-  const handleInput = () => {
-    if(text.trim().length <= min) {
-      message = `Text must be at least ${min} characters`
-      btnDisabled = true
-    } else {
-      message = null
-      btnDisabled = false
-    }
-  }
-  const handleSubmit = () => {
-    if(text.trim().length > min) {
-      const newFeedback = {
-        id: uuidv4(),
-        text,
-        rating: +rating
-      }
-      FeedbackStore.update((currentFeedback) => {
-        return [newFeedback, ...currentFeedback]
-      })
-      text = ''
-    }
-  }
+  // let min = 10
+  // let message
+  // const handleSelect = e => rating = e.detail
+  // const handleInput = () => {
+  //   if(text.trim().length <= min) {
+  //     message = `Text must be at least ${min} characters`
+  //     btnDisabled = true
+  //   } else {
+  //     message = null
+  //     btnDisabled = false
+  //   }
+  // }
+  // const handleSubmit = () => {
+  //   if(text.trim().length > min) {
+  //     const newFeedback = {
+  //       id: uuidv4(),
+  //       text,
+  //       rating: +rating
+  //     }
+  //     FeedbackStore.update((currentFeedback) => {
+  //       return [newFeedback, ...currentFeedback]
+  //     })
+  //     text = ''
+  //   }
+  // }
 </script>
 
 
@@ -39,17 +39,19 @@
   <header>
     <h2>How would you rate your service with us?</h2>
   </header>
-<form on:submit|preventDefault={handleSubmit}>
-  <RatingSelect on:rating-select={handleSelect} />
+  <!-- on:submit|preventDefault={handleSubmit} -->
+<form >
+  <!-- <RatingSelect on:rating-select={handleSelect} /> -->
   <div class="input-group">
-    <input type="text" on:input={handleInput} bind:value = {text} placeholder="Tell us something that keeps you coming back">
+    <!-- on:input={handleInput} -->
+    <input type="text"  bind:value = {text} placeholder="Tell us something that keeps you coming back">
     <Button disabled={btnDisabled} type="submit">Send</Button>
   </div>
-  {#if message}
+  <!-- {#if message}
     <div class="message">
       {message}
     </div>
-  {/if}
+  {/if} -->
 </form>
 </Card>
 
@@ -79,9 +81,9 @@
   input:focus {
     outline: none;
   }
-  .message{
+  /* .message{
     padding-top: 10px;
     text-align: center;
     color: rebeccapurple;
-  }
+  } */
 </style>
